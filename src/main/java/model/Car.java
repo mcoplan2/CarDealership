21be838f3254b7b99ce7705044162a5e2217e448
Matthew Coplan <mcoplanwork@gmail.com>
2022-06-05 19:59:51 -0400

    created Car Model

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,4 +1,21 @@
 package model;
 
 public class Car {
+    public String make;
+    public String model;
+    public int year;
+
+    public Car(){}
+
+    public Car(String make, String model, int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    @Override
+    public String toString() {
+        return "Year: " + year + "\n" +
+                "Make: " + make + "\n" +
+                "Model: " + model + "\n\n";
+    }
 }
