62e31450b13cd3a1f93b070efe13d8a1c7dbce04
Matthew Coplan <mcoplanwork@gmail.com>
2022-06-06 11:36:38 -0400

    added Enums to Car Model and gitignore file

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,21 +1,31 @@
 package model;
 
 public class Car {
+    public enum Status{
+        AVAILABLE, TAKEN;
+    }
     public String make;
     public String model;
     public int year;
+    public Status status;
 
     public Car(){}
 
-    public Car(String make, String model, int year){
+    public Car(Status status){
+        this.status = status;
+    }
+
+    public Car(String make, String model, int year, Status status){
         this.make = make;
         this.model = model;
         this.year = year;
+        this.status = status;
     }
     @Override
     public String toString() {
         return "Year: " + year + "\n" +
                 "Make: " + make + "\n" +
-                "Model: " + model + "\n\n";
+                "Model: " + model + "\n" +
+                "Status: " + status + "\n\n";
     }
 }
