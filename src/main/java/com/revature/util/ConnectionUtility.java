5a31a316b5c5bb6fc0b8b546324f40ff73879c8b
Matthew Coplan <mcoplanwork@gmail.com>
2022-06-17 16:18:42 -0400

    Refactored to hide Database information in a properties file, and added a testcase to test functionality.

--- src/main/java/com/revature/util/ConnectionUtility.java
+++ src/main/java/com/revature/util/ConnectionUtility.java
@@ -1,23 +1,46 @@
 package com.revature.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionUtility {
 
     private ConnectionUtility(){}
     public static Connection instance;
-
-    private final static String url = "";
-    private final static String user = "";
-    private final static String pass = "";
+    private static Properties properties;
 
     public static Connection getConnection() throws SQLException {
 
+        // load the properties if not loaded
+        if(properties == null) {
+            properties = loadProperties();
+        }
+
+        // check
         if(instance == null || instance.isClosed()) {
-            instance = DriverManager.getConnection(url, user, pass);
+            instance = DriverManager.getConnection(
+                    properties.getProperty("url"),
+                    properties.getProperty("user"),
+                    properties.getProperty("password"));
         }
         return instance;
+    }
+
+    public static Properties loadProperties() {
+        Properties properties = new Properties();
+
+        try{
+            FileInputStream fileInputStream = new FileInputStream("src/main/resources/db-config.properties");
+            properties.load(fileInputStream);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
     }
 }
--- src/test/java/com/revature/util/ConnectionUtilityTest.java
+++ src/test/java/com/revature/util/ConnectionUtilityTest.java
@@ -1,2 +1,29 @@
-package com.revature.util;public class ConnectionUtilityTest {
+package com.revature.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtilityTest {
+
+    @Test
+    public void whenGivenValidCredentialsGetConnectionReturnsValidConnection() throws SQLException {
+        Connection connection = ConnectionUtility.getConnection();
+
+        Assertions.assertNotNull(connection);
+    }
+    @Test
+    public void whenLoadingPropertiesLoadsValidProperties(){
+        String url="jdbc:postgresql://localhost:5432/postgres?currentSchema=project1";
+        String user="postgres";
+        String password="password";
+        Properties properties = ConnectionUtility.loadProperties();
+
+        Assertions.assertEquals(url, properties.getProperty(url));
+        Assertions.assertEquals(user, properties.getProperty(user));
+        Assertions.assertEquals(password, properties.getProperty(password));
+    }
 }
